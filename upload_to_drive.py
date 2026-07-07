092c48a9d3a1421d9b84519c1ca4e88ffb7e7cd1
joergneuber <joerg.neuber@googlemail.com>
2026-07-07 14:55:11 +0200

    upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -2,6 +2,7 @@ import os
 import json
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 
 def get_drive_service():
     token_str = os.environ.get("GDRIVE_TOKEN")
