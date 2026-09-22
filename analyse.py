aee1353e922f6bc69835c51f2af425c07761b591
joergneuber <joerg.neuber@googlemail.com>
2026-09-22 06:58:09 +0200

    analyse.py aktualisieren

--- analyse.py
+++ analyse.py
@@ -685,7 +685,7 @@ eu_benchmark_ticker = "EXSA.DE"  # iShares STOXX Europe 600 UCITS ETF (DE) - EU-
 # gelegentlich gegenchecken)
 dax_aktien = {
     "Industrie": [
-        "10O.DE", "1TRA.DE", "AIR.DE", "ASTA.DE", "AUM.DE", "BFSA.DE", "BMW.DE", "BNR.DE",
+        "10O.DE", "8TRA.DE", "AIR.DE", "ASTA.DE", "AUM.DE", "BFSA.DE", "BMW.DE", "BNR.DE",
         "CON.DE", "DEZ.DE", "DHL.DE", "DTG.DE", "DUE.DE", "FRAG.DE", "G1A.DE", "GBF.DE", "HAG.DE",
         "HDD.DE", "HEI.DE", "HOT.DE", "JST.DE", "JUN3.DE", "KBX.DE", "KGX.DE", "KRN.DE", "KSB3.DE",
         "LHA.DE", "MBB.DE", "MBG.DE", "MTX.DE", "NDX1.DE", "NOEJ.DE", "P911.DE", "R3NK.DE",
@@ -693,7 +693,7 @@ dax_aktien = {
         "VOS.DE", "VOW3.DE", "WAC.DE"
     ],
     "Automobil": [
-        "10O.DE", "1TRA.DE", "AUM.DE", "BMW.DE", "CON.DE", "DTG.DE", "JST.DE", "KBX.DE", "MBG.DE",
+        "10O.DE", "8TRA.DE", "AUM.DE", "BMW.DE", "CON.DE", "DTG.DE", "JST.DE", "KBX.DE", "MBG.DE",
         "PAH3.DE", "RHM.DE", "SFQ.DE", "SHA.DE", "STM1.DE", "VOW3.DE", "P911.DE"
     ],
     "Chemie": [
